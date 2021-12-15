010db87f65002dae6daf3c758e53b9cac3a45e0a
Yan Rodrigues <ccyan05@alu.ufc.br>
2021-12-15 16:15:46 -0300

    avl test file

--- src/index.c
+++ src/index.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include "./lib/avl.h"
 
 int main(int argc, char** argv) {
-  printf("Olá, mundo!");
+  Avl* a = CreateAvl();
+  Insert(a, 10);
+  Insert(a, 4);
+  Insert(a, 89);
+  Insert(a, 12);
+  Insert(a, 3);
+  Insert(a, 7);
+  Insert(a, 20);
+  Insert(a, 30);
+  Insert(a, 40);
+  Insert(a, 17);
+  Insert(a, 9);
+  Insert(a, 1);
+  Insert(a, 15);
+  Insert(a, 8);
+
+  InOrder(a->root);
+
+  printf("\nOlá, mundo!");
   return 0;
 }
