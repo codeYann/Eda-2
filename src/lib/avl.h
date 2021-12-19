717280ca4fa914eb77a8f430772e0a6299a92b9d
Yan Rodrigues <ccyan05@alu.ufc.br>
2021-12-19 18:51:53 -0300

    Remove nodes function

--- src/lib/avl.h
+++ src/lib/avl.h
@@ -2,27 +2,31 @@
 #ifndef AVL
 #define AVL
 
-typedef struct node {
-  struct node* left;
-  struct node* right;
+typedef struct node
+{
+  struct node *left;
+  struct node *right;
   int key;
   int balancing;
 } Node;
 
-typedef struct {
-  Node* root;
+typedef struct
+{
+  Node *root;
   int length;
 } Avl;
 
-Node* CreateNode(int key);
-Avl* CreateAvl();
-void Insert(Avl* tree, int key);
+Node *CreateNode(int key);
+Avl *CreateAvl();
+void Insert(Avl *tree, int key);
+void Delete(Avl *tree, int key);
 
-void PreOrder(Node* root);
-void InOrder(Node* root);
-void PostOrder(Node* root);
+void PreOrder(Node *root);
+void InOrder(Node *root);
+void PostOrder(Node *root);
 
-bool IsAVL(Avl* tree);
-int SizeAvl(Avl* tree);
+bool IsAVL(Avl *tree);
+int SizeAvl(Avl *tree);
+void ClearNodes(Node **root);
 
 #endif
