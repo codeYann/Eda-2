78256609e48d0b043b3763d7d0b0fccd5f9687f0
codeYann <ccyan05@alu.ufc.br>
2022-02-19 15:33:36 -0300

    Refactoring insert and remove

--- src/avl_insert_test.c
+++ src/avl_insert_test.c
@@ -1,29 +1,26 @@
-#include <stdio.h>
 #include "./lib/avl.h"
+#include <stdio.h>
 
 int list1[] = {10, 4, 89, 12, 3, 7, 20, 30, 40, 17, 9, 1, 15, 8};
 int list2[] = {18, 16, 14, 12, 10, 1, 22, 24, 26, 30, 32};
 int list3[] = {24, 89, 1, 11, 0, 169, 2871, 12, 144, 121, 7, 15};
 
-
-void TestInsertAvl(Avl *tree, int *list, int n) 
-{
+void test_insert_avl(Avl *tree, int *list, int n) {
   for (int i = 0; i < n; i++) {
     Insert(tree, list[i]);
   }
-  InOrder(tree->root);
-  printf("Árvore é uma avl? => %d\n", IsAVL(tree));
-  printf("Quantidade de nós => %d\n\n", SizeAvl(tree));
+  in_order(tree->root);
+  printf("Árvore é uma avl? => %d\n", is_avl(tree));
+  printf("Quantidade de nós => %d\n\n", size_avl(tree));
 }
 
-int main(int argc, char **argv)
-{
-  Avl *a = CreateAvl();
-  Avl *b = CreateAvl();
-  Avl *c = CreateAvl();
+int main(int argc, char **argv) {
+  Avl *a = create_avl();
+  Avl *b = create_avl();
+  Avl *c = create_avl();
 
-  TestInsertAvl(a, list1, 14);
-  TestInsertAvl(b, list2, 11);
-  TestInsertAvl(c, list3, 12);
+  test_insert_avl(a, list1, 14);
+  test_insert_avl(b, list2, 11);
+  test_insert_avl(c, list3, 12);
   return 0;
 }
--- src/avl_remove_test.c
+++ src/avl_remove_test.c
@@ -1,36 +1,34 @@
-#include <stdio.h>
 #include "./lib/avl.h"
+#include <stdio.h>
 
-void TestInsertAvl(Avl *tree, int *list, int n) 
-{
+void test_insert_avl(Avl *tree, int *list, int n) {
   for (int i = 0; i < n; i++) {
     Insert(tree, list[i]);
   }
 
-  InOrder(tree->root);
-  printf("Árvore é uma avl? => %d\n", IsAVL(tree));
-  printf("Quantidade de nós => %d\n\n", SizeAvl(tree));
+  in_order(tree->root);
+  printf("Árvore é uma avl? => %d\n", is_avl(tree));
+  printf("Quantidade de nós => %d\n\n", size_avl(tree));
 }
 
-int main(int argc, char **argv)
-{
-  Avl *a = CreateAvl();
+int main(int argc, char **argv) {
+  Avl *a = create_avl();
 
   int list[] = {10, 4, 89, 12, 3, 7, 20, 30, 40, 17, 9, 1, 15, 8};
   int list2[] = {18, 16, 14, 12, 10, 1, 22, 24, 26, 30, 32};
   int list3[] = {24, 89, 1, 11, 0, 169, 2871, 12, 144, 121, 7, 15};
 
-  TestInsertAvl(a, list3, 12);
-  
+  test_insert_avl(a, list3, 12);
+
   printf("Removendo 12 e 7\n");
   Delete(a, 12);
   Delete(a, 7);
   // Delete(a, 11); Erro ao deleter um nó com 2 filhos.
 
   printf("\n");
-  InOrder(a->root);
-  printf("Árvore é uma avl? => %d\n", IsAVL(a));
-  printf("Quantidade de nós => %d\n", SizeAvl(a));
+  in_order(a->root);
+  printf("Árvore é uma avl? => %d\n", is_avl(a));
+  printf("Quantidade de nós => %d\n", size_avl(a));
 
   return 0;
 }
