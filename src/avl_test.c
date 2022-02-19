a0c311c40f351a534ba17637428862240c34428a
codeYann <ccyan05@alu.ufc.br>
2022-02-19 15:33:08 -0300

    Refactoring avl_test

--- src/avl_test.c
+++ src/avl_test.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "./lib/avl.h"
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 #define LENGTH 10000
 
-bool checkNumber(int* list, int n, int number)
-{
+bool check_number(int *list, int n, int number) {
   int temp = list[0];
   for (int i = 1; i < n; i++) {
     if (temp == number) {
@@ -18,8 +17,7 @@ bool checkNumber(int* list, int n, int number)
   return false;
 }
 
-int *listFill(int n)
-{
+int *list_fill(int n) {
   int repair = 200001;
   int semente;
   int *list = malloc(sizeof(int) * n);
@@ -27,7 +25,7 @@ int *listFill(int n)
   for (int i = 0; i < n; i++) {
     semente = (rand() % n);
     list[i] = semente;
-    if (checkNumber(list, n, semente) == true) {
+    if (check_number(list, n, semente) == true) {
       list[i] = repair;
       repair += 1;
     }
@@ -35,25 +33,23 @@ int *listFill(int n)
   return list;
 }
 
-void AvlInsert(Avl *tree, int *list, int n) 
-{
+void avl_insert_test(Avl *tree, int *list, int n) {
   for (int i = 0; i < n; i++) {
     Insert(tree, list[i]);
   }
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   Avl *tree;
   int *list;
   for (int i = 0; i < LENGTH; i++) {
-    tree = CreateAvl();
-    list = listFill(LENGTH);
-    AvlInsert(tree, list, LENGTH);
+    tree = create_avl();
+    list = list_fill(LENGTH);
+    avl_insert_test(tree, list, LENGTH);
 
-    printf("\nA árvore é avl? => %d\n", IsAVL(tree));
-    printf("Tamanho da árvore avl => %d\n", SizeAvl(tree));
-    ClearNodes(&tree->root);   
+    printf("\nA árvore é avl? => %d\n", is_avl(tree));
+    printf("Tamanho da árvore avl => %d\n", size_avl(tree));
+    clear_nodes(&tree->root);
     free(tree);
     free(list);
   }
